cd7f52cb6cff063442bd78d58ddb55ce2d6ad76c
NashJanaban <nashjanaban50@gmail.com>
2026-02-14 23:15:16 +0800

    Update settings.py

--- valentines/settings.py
+++ valentines/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mcfq-dg5-pvn4l3w8o6!!=zz06u1htyo(rw2%b&h7+zws@xsfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
+"ALLOWED_HOSTS = ['*']"
 "ALLOWED_HOSTS = ['.ngrok-free.dev', 'localhost', '127.0.0.1']"
 
 
